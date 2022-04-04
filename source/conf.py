5f2e76c4bd64787ba034c4f63cbd71702d7a0a7e
paulsweet <paul_sweet@126.com>
2022-04-04 20:43:19 +0800

    red sphinx theme

--- source/conf.py
+++ source/conf.py
@@ -71,11 +71,11 @@ master_doc = 'index'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# html_context = {
-#     "display_github": True, # Integrate GitHub
-#     "github_user": "paulsweet", # Username
-#     "github_repo": "note_2022", # Repo name
-#     "github_version": "master", # Version
-#     "conf_py_path": "/source/", # Path in the checkout to the docs root
-# }
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "paulsweet", # Username
+    "github_repo": "note_2022", # Repo name
+    "github_version": "master", # Version
+    "conf_py_path": "/source/", # Path in the checkout to the docs root
+}
 
